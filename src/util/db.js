b0f836b9cf72e30b78dc8c5b1f916adc3c6214d3
mario gerges <mario85.girges@gmail.com>
2026-08-19 05:17:41 -0700

    creating db util file

--- src/util/db.js
+++ src/util/db.js
@@ -1,20 +1,16 @@
-// const Sequelize = require("sequelize")
-// const mysql = require("mysql2")
-// const dotenv = require("dotenv")
+const mysql = require("mysql2");
+// const dotenv = require("dotenv").config();
+const Sequelize = require("sequelize");
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     dialect: "mysql",
-//     port: process.env.DB_PORT,
-//     logging: false,
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }   
-// })
-console.log(process.env.PORT);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  },
+);
 
-
-// module.exports = sequelize;
+module.exports = sequelize;
