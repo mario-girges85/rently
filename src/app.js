4dbd4e30a1f1a2e7a9c9ece8819a973654d76d04
mario gerges <mario85.girges@gmail.com>
2026-08-19 05:17:35 -0700

    creating db util

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.get("/", (req, res) => {
+    res.send("Server is running on port 3000");
+});
 
 module.exports = app;
